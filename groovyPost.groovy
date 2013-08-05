6a8852e6fc0f69e9bd9dadcb1f5642ee9cf5717e
Jesse Plymale <jesse@languagecomputer.com>
2013-08-05 14:51:45 -0500

    Script to help with posting

---
+++ groovyPost.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+String title = args[0]
+String cleanedTitle = title.replaceAll(~/[^a-zA-Z ]/, "")
+String titleWithDashes = cleanedTitle.replace(' ', '-').toLowerCase()
+String dateString = new Date().format('yyyy-MM-dd')
+String fileName = "_posts/${dateString}-${titleWithDashes}.markdown".toString()
+println "Creating file: ${fileName}"
+
+new File(fileName).text = """---
+layout: groovyPost
+title: ${title}
+tags: groovyExamples
+categories: groovy
+---
+
+"""
